acdadbbf64441b43f496c824088a4e4cee8f40c1
nickadamson <naburner@protonmail.com>
2021-08-31 19:30:51 -0700

    public

---
+++ src/mappings/factory.ts
@@ -0,0 +1,28 @@
+import { BigInt } from "@graphprotocol/graph-ts"
+import { ProxyCreated } from "../../generated/OurFactory/OurFactory"
+import { OurProxy } from "../../generated/schema"
+import { findOrCreateUser } from "./helpers";
+
+/**
+ * Handler called when the `ProxyCreated` Event is emitted on OurFactory
+ * @eventParam address ourProxy: address of the newly created contract
+ * @eventParam address proxyOwner: address of the user who created and now manages the proxy
+ * @eventParam string splitRecipients: JSON.stringify([array]) of the Split's recipients
+ */
+export function handleProxyCreated(event: ProxyCreated): void {
+  let proxyAddress = event.params.ourProxy.toHexString();
+  let owner = findOrCreateUser(event.params.proxyOwner.toHexString());
+  let splitRecipients = event.params.splitRecipients;
+
+  let ourProxy = new OurProxy(proxyAddress);
+
+  ourProxy.transactionHash = event.transaction.hash.toHexString();
+  ourProxy.createdAtTimestamp = event.block.timestamp;
+  ourProxy.createdAtBlockNumber = event.block.number;
+  ourProxy.creator = owner.id;
+  ourProxy.transfers = BigInt.fromI32(0);
+  ourProxy.splitRecipients = splitRecipients;
+  ourProxy.ETH = BigInt.fromI32(0);
+
+  ourProxy.save()
+}
