5a2109bcece660194ca006596d5221b68d6a10af
nickadamson <naburner@protonmail.com>
2021-10-18 20:05:19 -0700

    [fix] include all nft-components style settings

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,5 +1,6 @@
 import { MediaConfiguration } from "@zoralabs/nft-components";
 import { NFTFetchConfiguration, Networks } from "@zoralabs/nft-hooks";
+import { css } from "@emotion/react";
 import web3 from "@/app/web3";
 
 export default function GlobalProvider({ children }: { children: JSX.Element }): JSX.Element {
@@ -9,45 +10,75 @@ export default function GlobalProvider({ children }: { children: JSX.Element }):
         <MediaConfiguration
           networkId="4"
           style={{
+            styles: {
+              cardAuctionPricing: (theme, { type }) => {
+                // eslint-disable-next-line consistent-return
+                const getActiveStyle = () => {
+                  // eslint-disable-next-line default-case
+                  switch (type) {
+                    case "reserve-active":
+                      return `
+                        background: #F68D15;
+                        color: #fff;
+                      `;
+                    case "reserve-pending":
+                      return `
+                        background: #060606; 
+                      `;
+                    case "unknown":
+                      return ``;
+                    case "perpetual":
+                      return ``;
+                  }
+                };
+                return css`
+                  display: grid;
+                  grid-auto-flow: column;
+                  grid-template-rows: auto auto;
+                  grid-auto-columns: 1fr;
+                  padding: ${theme.textBlockPadding};
+                  border-top: ${theme.borderStyle};
+                  ${getActiveStyle()};
+                `;
+              },
+            },
             theme: {
+              previewCard: {
+                width: "330px",
+                height: "330px",
+                background: "#060606", // dark-background
+              },
+              nftProposalCard: {
+                mediaWidth: ";",
+                mediaHeight: ";",
+              },
+              // showCreator: true,
+              // showOwner: true,
+              showTxnLinks: true,
+              // spacingUnit: "20px",
+              textBlockPadding: "10px 15px",
               borderStyle: "1px solid #4D4D4D", // dark-border
-              defaultBorderRadius: 0,
               lineSpacing: 28,
-              /*
-               * headerFont: "color: #691900;",
-               * titleFont: "color: #FF7246;", //dark-primary
-               */
-              bodyFont: "color: #FFFFFF;", // dark-primary
-              linkColor: "#FF7246", // ourange-300
+              maximumPricingDecimals: 4,
+              linkColor: "#B2B2B2", // ourange-300
+              bodyFont: "color: #FFF;", // dark-primary
+              titleFont: "color: #F68D15;", // 70% gray
+              headerFont: "color: #691900;",
+              mediaContentFont: {
+                fontFamily: `'Playfair Display', 'serif';`,
+              },
               buttonColor: {
                 primaryBackground: "#FFF",
                 primaryText: "#000",
                 background: "#000",
               },
-              previewCard: {
-                background: "#060606", // dark-background
-                height: "330px",
-                width: "330px",
-              },
-              /*
-               * titleFont: {
-               *   color: "#fff",
-               * },
-               * mediaContentFont: {
-               *   color: "#fff",
-               * },
-               */
+              defaultBorderRadius: 0,
+              fontSizeFull: 16,
+              // audioColors: {
+              //   waveformColor: '#999',
+              //   progressColor: '#333'
+              // }
             },
-            /*
-             * styles: {
-             *   cardAuctionPricing: {
-             *     display: "hidden",
-             *   },
-             *   cardItemInfo: {
-             *     display: "hidden",
-             *   },
-             * },
-             */
           }}
         >
           {children}
