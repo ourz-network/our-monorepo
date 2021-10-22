fac3c98fcd16abf44cda134d060c9c38292a6f9d
nickadamson <naburner@protonmail.com>
2021-10-21 21:56:01 -0700

    [feat] mainnet subgraph

--- src/modules/subgraphs/ourz/index.tsx
+++ src/modules/subgraphs/ourz/index.tsx
@@ -1,7 +1,10 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 // Ourz Rinkeby subgraph
-const APIURL = "https://api.thegraph.com/subgraphs/name/nickadamson/ourzrinkebyv1";
+// const APIURL = "https://api.thegraph.com/subgraphs/name/nickadamson/ourzrinkebyv1";
+
+// Ourz Mainnet subgraph
+const APIURL = `https://api.thegraph.com/https://gateway.thegraph.com/api/${process.env.SUBGRAPH_KEY}/subgraphs/id/0x11cdfcb54576d5990219c426bf2c630115a2012a-0`;
 
 // Create client
 const ourzSubgraph = new ApolloClient({
