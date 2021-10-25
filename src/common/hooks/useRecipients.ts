db2fdc6cde7ca17a851797950e22864c7ad70245
nickadamson <naburner@protonmail.com>
2021-10-25 14:33:05 -0700

    [fix] error displaying non-split nfts

--- src/common/hooks/useRecipients.ts
+++ src/common/hooks/useRecipients.ts
@@ -1,36 +1,38 @@
 import { useState, useEffect } from "react";
-import { SplitRecipient } from "@/utils/OurzSubgraph";
+import { Recipient } from "@/utils/OurzSubgraph";
 import { ChartData } from "../components/Charts/DetailedPie";
 
 const useRecipients = ({
   recipients,
   secondaryRoyalty,
 }: {
-  recipients: SplitRecipient[];
+  recipients: Recipient[];
   secondaryRoyalty: number | undefined;
 }) => {
   const [firstSale, setFirstSale] = useState<ChartData[] | null>(); // 100% of funds from sale are split
   const [secondarySales, setSecondarySales] = useState<ChartData[] | null>(); // only get x% of funds from sale
 
   useEffect(() => {
-    // create first sale chart data
-    let newChartData = recipients.flatMap((recipient) => ({
-      name: `${recipient.name || recipient.user.id}`,
-      shares: Number(recipient.shares),
-    }));
-    setFirstSale(newChartData);
-
-    if (secondaryRoyalty) {
-      newChartData = newChartData.map((recipient) => ({
-        name: recipient.name,
-        shares: Number(recipient.shares) * Number((secondaryRoyalty / 100).toFixed(4)),
+    if (recipients) {
+      // create first sale chart data
+      let newChartData = recipients.flatMap((recipient) => ({
+        name: `${recipient.name || recipient.user.id}`,
+        shares: Number(recipient.shares),
       }));
-      newChartData.push({
-        name: "Owner",
-        shares: 100 - secondaryRoyalty,
-      });
+      setFirstSale(newChartData);
 
-      setSecondarySales(newChartData);
+      if (secondaryRoyalty) {
+        newChartData = newChartData.map((recipient) => ({
+          name: recipient.name,
+          shares: Number(recipient.shares) * Number((secondaryRoyalty / 100).toFixed(4)),
+        }));
+        newChartData.push({
+          name: "Owner",
+          shares: 100 - secondaryRoyalty,
+        });
+
+        setSecondarySales(newChartData);
+      }
     }
   }, [recipients, secondaryRoyalty]);
 
